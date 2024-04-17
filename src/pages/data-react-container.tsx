c2aed0e28d456f8f98e27d93d79b82890839acbd
NourSb2024 <nour.sboui.email@gmail.com>
2024-04-17 07:45:39 +0200

    feat(trading-info): add home page, navigation & needed components

---
+++ src/pages/data-react-container.tsx
@@ -0,0 +1,12 @@
+import HomeIcon from "@/assets/icons/home-icon";
+import Link from "next/link";
+
+export default function DataReactContainer() {
+  return (
+    <div>
+      <Link href="/">
+        <HomeIcon />
+      </Link>
+    </div>
+  );
+}
